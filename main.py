7d1e4c0caece23aaef5e736b4033c43d1f587b74
mim <mimramcharn@gmail.com>
2021-03-22 00:43:43 +0400

    [integration] mettre a jour integration

---
+++ main.py
@@ -0,0 +1,9 @@
+from addTwolnt.py import *
+
+reponse=str(input("Voulez-vous additionner deux int? "))
+if reponse=="oui":
+   main()
+elif reponse=="non":
+     print("La reponse est non okay!")
+else:
+     print("La reponse n'est ni OUI ni NON!!!")
