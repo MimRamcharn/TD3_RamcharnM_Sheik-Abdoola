f9424a3ad2110b21911df80384d4f165f94dc71f
mim <mimramcharn@gmail.com>
2021-03-21 21:54:24 +0400

    [add-0.1]Arriver a l'etape 10

--- addTwolnt.py
+++ addTwolnt.py
@@ -1,11 +1,21 @@
-def add(a,b):
-    add=a+b;
-    return add;
+#!/usr/bin/env python
+
+def add_num(a,b):
+    sum=a+b;
+    return sum;
 
 def main():
     import sys
     print(sys.argv)
-    x=int(sys.argv[1])
-    y=int(sys.argv[2])
-    print(add(x,y))
+    i=(len(sys.argv)-1)
+    print("Le nombre d'arguments : ",i)
+
+
+
+    if (i==2):
+           x = int(sys.argv[1])
+           y = int (sys.argv[2])
+           print(add_num(x,y))
+    else:
+           print("erreur : veuillez inserer que deux arguments")
 main()
